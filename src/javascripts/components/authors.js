45cf745dd9dbcba6c98fb96fe718372715b2f4d6
david vareba <davidvareba@gmail.com>
2021-08-30 15:14:06 -0500

    completed MVP

--- src/javascripts/components/authors.js
+++ src/javascripts/components/authors.js
@@ -1,12 +1,22 @@
-// FIXME: STUDENTS show your authors
-
 const showAuthors = (array) => {
   document.querySelector('#store').innerHTML = '';
-  // CREATE A BUTTON TO ADD BOOKS
+  // CREATE A BUTTON TO ADD AUTHOR
+  document.querySelector('#add-button').innerHTML = '<button class="btn btn-success btn-lg mb-4" id="add-author-btn">Add An Author</button>';
+  document.querySelector('#form-container').innerHTML = '';
 
   array.forEach((item) => {
-    // FIXME: STUDENTS create cards for your authors
-    document.querySelector('#store').innerHTML += `${item}`;
+    document.querySelector('#store').innerHTML += `
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${item.first_name} ${item.last_name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${item.email}</h6>
+        <hr>
+        <i class="btn btn-success fas fa-eye" id="view-author-btn--${item.firebaseKey}"></i>
+        <i class="fas fa-edit btn btn-info" id="update-author--${item.firebaseKey}"></i>
+        <i class="btn btn-danger fas fa-trash-alt" id="delete-author--${item.firebaseKey}"></i>
+      </div>
+    </div>
+    `;
   });
 };
 
